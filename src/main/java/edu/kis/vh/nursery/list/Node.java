aae2bea96ff4d4bebf940a22e66cb2bb1605c832
251152 <251152@edu.p.lodz.pl>
2025-04-03 16:40:01 +0200

    Dodanie dokumentacji do klasy Node

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,5 +1,18 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Klasa reprezentująca węzeł (node) w strukturze danych typu lista podwójnie związana.
+ * Węzeł zawiera wartość oraz wskaźniki na poprzedni i następny węzeł w liście.
+ * 
+ * Atrybuty:
+ * - value: przechowuje wartość węzła typu całkowitego (int).
+ * - prev: wskaźnik na poprzedni węzeł w liście.
+ * - next: wskaźnik na następny węzeł w liście.
+ * 
+ * Konstruktor:
+ * - Node(int i): inicjalizuje węzeł z wartością i.
+ */
+
 public class Node {
 
     public final int value;
